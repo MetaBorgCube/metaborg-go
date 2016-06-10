816e48996b4c1e6e153910776636f1c6d008e7ed
Olaf Maas <olafmaas@users.noreply.github.com>
2016-06-10 09:41:31 +0200

    Adds full channel support

--- js/goroutine.js
+++ js/goroutine.js
@@ -1,27 +1,37 @@
 module.exports = {
 	go: go,
+	newChannel: function(size)
+	{
+		return new Channel(size);
+	},
+	end: function()
+	{
+		__scheduler.unregister();
+	}
 }
 
-//TODO: Main Go Routine Handling
-//TODO: Channels
+//TODO: Main Go Routine Handling 
+//TODO: Registration
 //TODO: Deadlock Detection
 //TODO: Stack Fixing
-//TODO: Buffered Channels
-//TODO: Non-Blocking Channels
+//TODO: Work stealing?
 //TODO: Think about panic
 //TODO: Think about defer
 
 var __scheduler = new Scheduler();
+var nrSleepingRoutines = 0;
 
-var __currentRoutine = new GoRoutine();
 
 var mainroutine = 0;
 
 function Scheduler()
 {
 
+	//TODO: channellist for Deadlock detection
 	var scheduled = []
 	var alive = false;
+
+	var totalRoutines = 1;
 
 	this.schedule = function(routine, direct)
 	{
@@ -43,10 +53,20 @@ function Scheduler()
 	{
 		alive = true;
 
-		//Use timeout, else this method would return then all GoRoutines are finished and the main 
+		//Use timeout, else this method would return when all GoRoutines are finished and the main 
 		//would block  which would defeat the purpose of the 
 		//Routine
 		setTimeout(this.executeNext, 1);
+	}
+
+	this.register = function()
+	{
+		totalRoutines++;
+	}
+
+	this.unregister = function()
+	{
+		totalRoutines--;
 	}
 
 	this.executeNext = function()
@@ -59,6 +79,12 @@ function Scheduler()
 				routine();
 			}
 
+			if(nrSleepingRoutines == totalRoutines)
+			{
+				console.log("All Go Routines are sleeping, deadlock detected.")
+			}
+
+			//TODO: Insert Deadlock Detection here
 			alive = false;
 		}finally{
 			//If something goes wrong in a routine keep scheduling
@@ -70,15 +96,92 @@ function Scheduler()
 	}	
 }
 
-function GoRoutine()
+function Channel(size)
 {
-	this.init = function(){
+	var buffer = [];
+	var buffersize = size 
 
+	//TODO: Check multiple waiting
+	var waitingReceivers = [];
+	var waitingSenders = [];
+
+	this.send = function(message, callback)
+	{
+		if(buffer.length <= buffersize)
+		{
+			if(waitingReceivers.length > 0)
+			{
+				var recCallback = waitingReceivers.shift()
+
+				//Release waiting routine 
+				scheduleReceiver(recCallback, message)
+				nrSleepingRoutines--;
+			}else{
+				buffer.push(message);
+			}
+			
+			__scheduler.schedule(callback)
+		}else{
+			nrSleepingRoutines++;
+			waitingSenders.push({
+				data: message,
+				callback: callback
+			});
+		}
 	}
-}
+
+	this.receive = function(callback)
+	{
+		var data = buffer.shift();
+		var sender = null;
+
+		if(waitingSenders.length > 0)
+		{
+			sender = waitingSenders.shift();
+			__scheduler.schedule(sender.callback)
+			nrSleepingRoutines--;
+		}
+
+		if(data !== undefined)
+		{
+			scheduleReceiver(callback, data)
+
+			//If the buffer was full, 
+			//senders could be waiting to send
+			if(sender){
+				 buffer.push(sender.data)
+			}
+		}else{
+			if(sender)
+			{
+				scheduleReceiver(callback, sender.data)
+			}else{
+
+				//Wait for message
+				waitingReceivers.push(callback);
+				nrSleepingRoutines++;
+			}
+		}
+	}
+
+	function scheduleReceiver(callback, data)
+	{
+		__scheduler.schedule(function(){
+			callback(data);
+		})
+	}
+
+	this.getNrOfWaitingRoutines = function()
+	{
+		return waitingSenders.length + waitingReceivers.length;
+	}
+
+
+} 
 
 function go(func)
 {
+	__scheduler.register();
 	__scheduler.schedule(func);
 }
 
