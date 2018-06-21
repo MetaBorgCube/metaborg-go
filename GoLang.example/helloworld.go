903db82c7ff88806749059ca4de2c727f1a88d54
Wim Spaargaren <wim@weave.nl>
2018-06-21 17:05:01 +0200

    Working sum

--- GoLang.example/helloworld.go
+++ GoLang.example/helloworld.go
@@ -3,22 +3,13 @@ package main;
 import "fmt";
   
 func main(){    
-		z := true;
-		if z {
-			fmt.Println("IK BEN EEN IF");
-		};
-		
-		test();
+		z := 1;
+		z = z + 1;
+		z = z + 5;
+	
+		fmt.Println(z);
 	
 };       
 
-func test(){
-	x := 4;
-	fmt.Println(x);
-	y := "Hello world 2";
-	fmt.Println(y);   
-	fmt.Println("yo");
-	fmt.Println(4); 
-};
 
                             
