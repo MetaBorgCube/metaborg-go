7377f1b63700dddd77a30d2b4f6fe803e81a1ebf
Wim Spaargaren <wim@weave.nl>
2018-06-21 15:36:18 +0200

    Now able to do multi var decl and multi var assign.

--- GoLang.example/helloworld.go
+++ GoLang.example/helloworld.go
@@ -10,12 +10,11 @@ func main(){
 		fmt.Println("yo");
 		fmt.Println(4); 
 		test();
-		bools();
+	
 };       
 
 func test(){
 	x := 4;
-	x = x + 4;
 	fmt.Println(x);
 	y := "Hello world 2";
 	fmt.Println(y);   
@@ -23,10 +22,4 @@ func test(){
 	fmt.Println(4); 
 };
 
-func bools(){
-	z := true;
-	if false {
-		fmt.Println("TRUE");
-	};
-};
                             
