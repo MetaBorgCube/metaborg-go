432895b0e04685b4a38a349f1dc7607a1ce7f00b
Wim Spaargaren <wim@weave.nl>
2018-06-21 21:15:54 +0200

    added product, difference + able to print multiple arguments + both print and println

--- GoLang.example/forloop.go
+++ GoLang.example/forloop.go
@@ -6,13 +6,12 @@ func main(){
 		i := 0;
 		j := 0;
 		for i < 10 {
+			j = i;
 			for j < 10 {
-				fmt.Println(i+1);
-				fmt.Println(j);
-				
+				fmt.Print("*");
 				j = j + 1;
 			};
-			j = 0;
+			fmt.Println("");
 			i = i + 1;
 		};
 };       
---
+++ GoLang.example/integers.go
@@ -0,0 +1,18 @@
+package main;
+
+import "fmt";
+  
+func main(){    
+		x,y := 4,5;
+		fmt.Println("x: ",x);
+		x = x + y;
+		fmt.Println("x = x + y",x );
+		x = x - y;
+		fmt.Println("x = x - y",x );
+		x = x * y;
+		fmt.Println("x = x * y" ,x);
+		fmt.Println("What was y?" );
+};       
+
+
+                            
