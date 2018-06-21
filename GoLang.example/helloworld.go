c0b3102f42e7d4f8377af7347793aa77690b78d1
Wim Spaargaren <wim@weave.nl>
2018-06-21 11:43:06 +0200

    Reenabled function calls.

--- GoLang.example/helloworld.go
+++ GoLang.example/helloworld.go
@@ -6,6 +6,18 @@ func main(){
 		x := 3;
 		fmt.Println(x);
 		y := "Hello world";
-		fmt.Println(y);    
+		fmt.Println(y);   
+		fmt.Println("yo");
+		fmt.Println(4); 
+		test();
 };       
+
+func test(){
+	x := 4;
+	fmt.Println(x);
+	y := "Hello world 2";
+	fmt.Println(y);   
+	fmt.Println("yo");
+	fmt.Println(4); 
+};
                             
