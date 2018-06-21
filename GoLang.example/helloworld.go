136a63e8d8cbef2a698f08893f788d1c0a29bcb7
Wim Spaargaren <wim@weave.nl>
2018-06-21 17:36:21 +0200

    Implemented less than.

--- GoLang.example/helloworld.go
+++ GoLang.example/helloworld.go
@@ -3,12 +3,21 @@ package main;
 import "fmt";
   
 func main(){    
-		z := 1;
-		z = z + 1;
-		z = z + 5;
-	
+		z := 4;
+		z = z + 6;
+		y := false;
+		y = z < 10;
 		fmt.Println(z);
-	
+		if y {
+			fmt.Println("z smaller than 10");
+		}else{
+			fmt.Println("z greater than 10");
+		};
+		i := 0;
+		for i < 20 {
+			fmt.Println(i);
+			i = i + 1;
+		};
 };       
 
 
