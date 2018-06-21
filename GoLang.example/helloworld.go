ee4c70f0b5662aa2c335e551dbee97f3c1e4bbba
Wim Spaargaren <wim@weave.nl>
2018-06-21 19:38:10 +0200

    example update.

--- GoLang.example/helloworld.go
+++ GoLang.example/helloworld.go
@@ -3,14 +3,28 @@ package main;
 import "fmt";
   
 func main(){    
-		x := 3;
-		fmt.Println(x);
-		y := "Hello world";
-		fmt.Println(y);   
-		fmt.Println("yo");
-		fmt.Println(4); 
-		test();
-	
+		z := 4;
+		z = z + 6;
+		y := false;
+		y = z < 10;
+		fmt.Println(z);
+		if y {
+			fmt.Println("z smaller than 10");
+		}else{
+			fmt.Println("z greater than 10");
+		};
+		i := 0;
+		j := 0;
+		for i < 10 {
+			for j < 10 {
+			fmt.Println(i);
+			fmt.Println(j);
+			
+			j = j + 1;
+			};
+			j = 0;
+			i = i + 1;
+		};
 };       
 
 func test(){
