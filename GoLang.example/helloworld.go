b08631b731dfebfc363c835a5bb9f45b4f31c6f2
Wim Spaargaren <wim@weave.nl>
2018-06-21 16:57:36 +0200

    Working forloop

--- GoLang.example/helloworld.go
+++ GoLang.example/helloworld.go
@@ -3,20 +3,11 @@ package main;
 import "fmt";
   
 func main(){    
-		x := 3;
 		z := true;
-		fmt.Println(x);
-		y := "Hello world";
-		fmt.Println(y);   
-		fmt.Println("yo");
-		fmt.Println(4); 
 		if z {
 			fmt.Println("IK BEN EEN IF");
 		};
-		for z {
-			fmt.Println("FOR");
-			z = false;
-		};
+		
 		test();
 	
 };       
