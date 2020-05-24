4abe59f841dea2ee705488a088f3dd5c2424b870
Daniel A. A. Pelsmaeker <647530+Virtlink@users.noreply.github.com>
2020-05-25 01:52:32 +0200

    More syntactic improvements and work on semantics

--- lang.go.example/src/test.go
+++ lang.go.example/src/test.go
@@ -1,27 +1,8 @@
 package main;
 
-import (
-    point "pkg";
-    "fmt";
-);
-
-type (
-  A1 = string;
-  A2 = A1;
-);
-
-type (
-  B1 string;
-  B2 B1;
-  B3 []B1;
-  B4 B3;
-);
+func test();
 
 func main() {
-    var x string = "Go";
-    x, x = "now", "now";
-    p := point.Point{1,1};
-    q := p.s#X;
-    d := p.s#Dist();
-    return "\n";
+    test();
 };
+
